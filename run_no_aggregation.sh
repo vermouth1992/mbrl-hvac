8b3185925031626a3f3903cfbd2bce9f00414a72
vermouth1992 <czhangseu@gmail.com>
2019-08-21 00:18:08 -0700

    run no aggregation script

---
+++ run_no_aggregation.sh
@@ -0,0 +1,4 @@
+python train_model_based.py --city SF --mpc_horizon 5 --num_days_on_policy 300 --training_epochs 200 --num_years 1
+python train_model_based.py --city Golden --mpc_horizon 5 --num_days_on_policy 300 --training_epochs 200 --num_years 1
+python train_model_based.py --city Chicago --mpc_horizon 5 --num_days_on_policy 300 --training_epochs 200 --num_years 1
+python train_model_based.py --city Sterling --mpc_horizon 5 --num_days_on_policy 300 --training_epochs 200 --num_years 1
